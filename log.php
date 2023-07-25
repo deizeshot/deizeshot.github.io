fe0d5bc91f47695b05f4f689b30529dd5428f054
deizeshot <deizeshot@gmail.com>
2023-07-25 13:44:36 +0300

    13

--- log.php
+++ log.php
@@ -1,10 +1,23 @@
 <?php
-// Получение сообщения из POST-запроса
-if (isset($_POST['message'])) {
-    $message = $_POST['message'];
+// Проверяем, что запрос пришел методом POST и содержит поле "message"
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['message'])) {
+  $message = $_POST['message'];
 
-    // Логируем сообщение в файл chat.log
-    $timestamp = date('Y-m-d H:i:s');
-    $logEntry = "[$timestamp] $message\n";
-    file_put_contents('chat.log', $logEntry, FILE_APPEND);
+  // Открываем файл для записи
+  $file = fopen('chat.log', 'a');
+  if ($file) {
+    // Записываем сообщение в файл
+    fwrite($file, $message . "\n");
+    fclose($file);
+
+    // Отправляем ответ об успешной записи
+    echo json_encode(['status' => 'success', 'message' => 'Message is logged.']);
+  } else {
+    // Ошибка при открытии файла
+    echo json_encode(['status' => 'error', 'message' => 'Unable to open file for writing.']);
+  }
+} else {
+  // Неверный метод запроса или отсутствует поле "message"
+  echo json_encode(['status' => 'error', 'message' => 'Invalid request.']);
 }
+?>
