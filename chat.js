21841aa185f74dabe13324ec505862883e6b0abf
deizeshot <deizeshot@gmail.com>
2023-07-25 17:34:22 +0300

    123

--- chat.js
+++ chat.js
@@ -21,23 +21,28 @@ ws.onmessage = (event) => {
 function displayMessage(message) {
   const chatMessages = document.getElementById('chatMessages');
   const messageDiv = document.createElement('div');
-  messageDiv.textContent = message.text;
+  messageDiv.classList.add('chat-message');
+
+  // Добавляем текст сообщения с пробелом и временем
+  const messageTextWithTime = document.createElement('div');
+  messageTextWithTime.textContent = message.text + ' ' + message.time;
+  messageDiv.appendChild(messageTextWithTime);
 
   // Добавляем класс в зависимости от типа сообщения (отправленное или полученное)
   if (message.type === 'chat') {
-    messageDiv.classList.add('chat-message', 'sent'); // Здесь можно использовать класс 'received' для полученных сообщений
+    messageDiv.classList.add('sent'); // Здесь можно использовать класс 'received' для полученных сообщений
   }
 
   chatMessages.appendChild(messageDiv);
   chatMessages.scrollTop = chatMessages.scrollHeight;
-
-
 }
 
-
-
-function sendMessageToServer(message) {
-  ws.send(JSON.stringify(message));
+// Функция для получения текущего времени в формате "HH:mm"
+function getTimeString() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  return `${hours}:${minutes}`;
 }
 
 // Пример отправки сообщения на сервер при клике на кнопку "Отправить"
@@ -51,3 +56,17 @@ sendButton.addEventListener('click', () => {
     messageInput.value = '';
   }
 });
+
+function sendMessageToServer(message) {
+  const currentTime = getTimeString(); // Получаем текущее время в формате "HH:mm"
+  message.time = currentTime; // Добавляем поле "time" с текущим временем в объект сообщения
+  ws.send(JSON.stringify(message));
+}
+
+// Функция для получения текущего времени в формате "HH:mm"
+function getTimeString() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  return `${hours}:${minutes}`;
+}
