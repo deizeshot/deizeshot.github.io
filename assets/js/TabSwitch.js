6120e6e8f78833ab2b08cadbf76016c11eee1dfa
deizeshot <deizeshot@gmail.com>
2023-07-27 15:07:24 +0300

    Update TabSwitch.js

--- assets/js/TabSwitch.js
+++ assets/js/TabSwitch.js
@@ -1,45 +1,45 @@
-// Функция для установки активной вкладки по параметру URL или из Local Storage
-function setActiveTab() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const activeTab = urlParams.get("tab") || localStorage.getItem("activeTab");
+// // Функция для установки активной вкладки по параметру URL или из Local Storage
+// function setActiveTab() {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const activeTab = urlParams.get("tab") || localStorage.getItem("activeTab");
   
-    if (activeTab) {
-      const tabs = document.querySelectorAll(".nav-link");
-      tabs.forEach((tab) => {
-        tab.classList.remove("active");
-      });
-      const activeTabElement = document.querySelector(`[href="${activeTab}"]`);
-      if (activeTabElement) {
-        activeTabElement.classList.add("active");
-      }
+//     if (activeTab) {
+//       const tabs = document.querySelectorAll(".nav-link");
+//       tabs.forEach((tab) => {
+//         tab.classList.remove("active");
+//       });
+//       const activeTabElement = document.querySelector(`[href="${activeTab}"]`);
+//       if (activeTabElement) {
+//         activeTabElement.classList.add("active");
+//       }
   
-      // Показываем соответствующий экран, если используется Bootstrap табы
-      const tabContent = document.querySelector(".tab-content");
-      if (tabContent) {
-        tabContent.querySelector(".active").classList.remove("active", "show");
-        const activeContent = document.querySelector(activeTab);
-        if (activeContent) {
-          activeContent.classList.add("active", "show");
-        }
-      }
-    }
-  }
+//       // Показываем соответствующий экран, если используется Bootstrap табы
+//       const tabContent = document.querySelector(".tab-content");
+//       if (tabContent) {
+//         tabContent.querySelector(".active").classList.remove("active", "show");
+//         const activeContent = document.querySelector(activeTab);
+//         if (activeContent) {
+//           activeContent.classList.add("active", "show");
+//         }
+//       }
+//     }
+//   }
   
-  // Вызываем функцию при загрузке страницы
-  window.addEventListener("DOMContentLoaded", setActiveTab);
+//   // Вызываем функцию при загрузке страницы
+//   window.addEventListener("DOMContentLoaded", setActiveTab);
   
-  // Сохраняем активный таб в параметре URL и в Local Storage при переключении
-  const tabs = document.querySelectorAll(".nav-link");
-  tabs.forEach((tab) => {
-    tab.addEventListener("click", () => {
-      const activeTab = tab.getAttribute("href");
-      const url = new URL(window.location.href);
-      url.searchParams.set("tab", activeTab.slice(1)); // Удаляем символ "#" из ID вкладки
-      window.history.replaceState({}, "", url);
+//   // Сохраняем активный таб в параметре URL и в Local Storage при переключении
+//   const tabs = document.querySelectorAll(".nav-link");
+//   tabs.forEach((tab) => {
+//     tab.addEventListener("click", () => {
+//       const activeTab = tab.getAttribute("href");
+//       const url = new URL(window.location.href);
+//       url.searchParams.set("tab", activeTab.slice(1)); // Удаляем символ "#" из ID вкладки
+//       window.history.replaceState({}, "", url);
   
-      localStorage.setItem("activeTab", activeTab); // Сохраняем активный таб в Local Storage
-    });
-  });
+//       localStorage.setItem("activeTab", activeTab); // Сохраняем активный таб в Local Storage
+//     });
+//   });
   
   // Дополнительные скрипты для переключения табов и сохранения активного таба
   const gamesButton = document.getElementById('gamesButton');
